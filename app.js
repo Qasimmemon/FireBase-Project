380203ca07c8ce0d657076f5f5c349b620fc6b74
Qasimmemon <qasimarif0321@gmail.com>
2024-07-30 09:32:25 +0500

    Update app.js

--- app.js
+++ app.js
@@ -64,15 +64,17 @@ function displayFoods(data) {
 
     const card = `
       <div id="food-${id}" class="flex flex-col items-center bg-white border border-gray-200 rounded-lg shadow md:flex-row md:max-w-xl hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700">
-        <img class="object-cover w-full rounded-t-lg h-96 md:h-auto md:w-48 md:rounded-none md:rounded-s-lg" style="margin-left: 0px;" src="${image}" alt="">
-        <div class="flex flex-col justify-between p-4 leading-normal">
-          <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">${foodName}</h5>
-          <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">${foodLocation}.</p>
-          <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">Added By ${addByEmail}</p>
-          <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">Price: ${foodPrice}</p>
-          <button id="delete-${id}" class="w-1/2 bg-red-500 text-white p-2 rounded">Delete</button>
-        </div>
+          <img class="object-cover w-full rounded-t-lg h-96 md:h-auto md:w-48 md:rounded-none md:rounded-s-lg" style="margin-left: 0px;" src="${image}" alt="">
+          <div class="flex flex-col justify-between p-4 leading-normal">
+            <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">${foodName}</h5>
+            <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">${foodLocation}.</p>
+            <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">Added By ${addByEmail}</p>
+            <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">Price: ${foodPrice}</p>
+            <div class="flex justify-start mt-4">
+        <button id="delete-${id}" class="bg-red-500 text-white p-2 rounded w-32 hover:bg-red-600 focus:outline-none focus:ring-2 focus:ring-red-500">Delete</button>
       </div>
+          </div>
+        </div>
     `;
 
     cards_container.innerHTML += card;
